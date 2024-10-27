6b38c591673def207983f92ddba8974ce5c35950
Cesar Munoz <likethesalad@gmail.com>
2024-10-27 17:12:31 +0100

    Setting up errorprone

--- asmifier-plugin/build.gradle.kts
+++ asmifier-plugin/build.gradle.kts
@@ -1,9 +1,15 @@
+import net.ltgt.gradle.errorprone.CheckSeverity
+import net.ltgt.gradle.errorprone.errorprone
+
 plugins {
     id("java-gradle-plugin")
+    id("net.ltgt.errorprone") version "4.1.0"
 }
 
 dependencies {
     implementation("org.ow2.asm:asm-util:9.7.1")
+    errorprone("com.google.errorprone:error_prone_core:2.35.1")
+    errorprone("com.uber.nullaway:nullaway:0.12.0")
     testImplementation(platform("org.junit:junit-bom:5.11.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.assertj:assertj-core:3.26.3")
@@ -11,6 +17,16 @@ dependencies {
 
 tasks.withType(Test::class.java) {
     useJUnitPlatform()
+}
+tasks.withType(JavaCompile::class.java) {
+    if (name.contains("test", true)) {
+        options.errorprone.isEnabled.set(false)
+    } else {
+        options.errorprone {
+            check("NullAway", CheckSeverity.ERROR)
+            option("NullAway:AnnotatedPackages", "com.likethesalad.asm")
+        }
+    }
 }
 
 gradlePlugin {
