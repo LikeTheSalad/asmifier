168b3a4f6a1c3405308b60e0c5d0b89d11d7b1db
Cesar Munoz <likethesalad@gmail.com>
2024-10-28 15:42:07 +0100

    Adding sample app

---
+++ sample-app/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("java")
+    id("com.likethesalad.asmifier") /* the version isn't needed for this project, but you must add it in yours */
+}
+
+dependencies {
+    asmifier("org.ow2.asm:asm-util:9.7.1")
+}
---
+++ sample-app/settings.gradle.kts
@@ -0,0 +1,10 @@
+rootProject.name = "asmifier-sample-app"
+
+dependencyResolutionManagement {
+    repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
+    repositories {
+        mavenCentral()
+    }
+}
+
+includeBuild("..")
