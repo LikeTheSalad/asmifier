7a90a36ae5016d872032bc0b7f3614a4ea64afac
Cesar Munoz <likethesalad@gmail.com>
2026-01-24 13:49:15 +0100

    Updating sample app

--- sample-app/build.gradle.kts
+++ sample-app/build.gradle.kts
@@ -3,6 +3,12 @@ plugins {
     id("com.likethesalad.asmifier") /* the version isn't needed for this project, but you must add it in yours */
 }
 
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(11)
+    }
+}
+
 dependencies {
     asmifier("org.ow2.asm:asm-util:9.9.1")
 }
